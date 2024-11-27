86f7bfe8409de334a7bc0ccf19a7289e4669940b
Aakash <aakash.ar1213@gmail.com>
2024-11-27 19:26:52 +0530

    Initiated the API in BE

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+require('dotenv').config();
+import Anthropic from "@anthropic-ai/sdk";
+
+
+const anthropic = new Anthropic();
+
+async function main(){
+    const msg = await anthropic.messages.create({
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 1000,
+        temperature: 0,
+        messages: [
+          {
+            "role": "user",
+            "content": "What is NextJS?"
+          }
+        ]
+      });
+      console.log(msg);
+}
+
+main();
